88c1000925ede67e840beace5c37785a5f26f9cc
Pravat-Lama <lpravat19@tbc.edu.np>
2023-01-17 14:11:30 +0545

    some error occured and couldn't solve so deleted again

--- component-2/routes/admin.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\AdminController;
-use Illuminate\Support\Facades\Route;
-
-Route::middleware('auth','is_admin')->group(function(){
- Route::get('/admin',[AdminController::class, 'index'])->name('admin.dashboard');
-});
